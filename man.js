bb0b6e5715855ec63e9b235a70b7dc4bc85fbf4a
NYCOLLE LIMA DOS SANTOS <23114984@cfp127aluno.local>
2023-08-16 17:07:26 -0300

    fazendo d

--- man.js
+++ man.js
@@ -2,6 +2,7 @@
 
 const botaoSomar = document.getElementById('somar')
 const botaoIdentificar = document.getElementById('identificar')
+const botaoMedia = document.getElementById('N1')
 
 function somar (){
     const numero1 = parseInt(document.getElementById('numero1').value)
@@ -27,5 +28,26 @@ function identificar (){
     }
 }
 
+function media (){
+    const nota1 = parseInt(document.getElementById('nota1').value)
+    const nota2 = parseInt(document.getElementById('nota2').value)
+    const nota3 = parseInt(document.getElementById('nota3').value)
+    const nota4 = parseInt(document.getElementById('nota4').value)
+
+    const media = (nota1 + nota2 + nota3 + nota4)/4
+
+
+    if (media > 7){
+        resultado.textContent = 'APROVADO'
+    } else if (media < 0){
+        resultado.textContent = 'REPROVADO'
+    } else {
+        resultado.textContent = 'APROVADO EM EXAME'
+    }
+
+    
+}
+
 botaoSomar.addEventListener('click', somar)
 botaoIdentificar.addEventListener('click', identificar)
+botaoMedia.addEventListener('click', N1)
