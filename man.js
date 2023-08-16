edf56145892484d024edd9ad5061f11c05f34c1c
NYCOLLE LIMA DOS SANTOS <23114984@cfp127aluno.local>
2023-08-16 15:38:33 -0300

    16-08

---
+++ man.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const botaoSomar = document.getElementById('somar')
+const botaoIdentificar = document.getElementById('identificar')
+
+function somar (){
+    const numero1 = parseInt(document.getElementById('numero1').value)
+    const numero2 = parseInt(document.getElementById('numero2').value)
+    const resultado = document.getElementById('resultado')
+
+    const total = numero1 + numero2
+
+    resultado.textContent = total
+}
+
+function identificar (){
+    const numero = parseInt(document.getElementById('numero-item2').value)
+    const resultado = document.getElementById('resultado-item2')
+
+
+    if (numero > 0){
+        resultado.textContent = 'POSITIVO'
+    } else if (numero < 0){
+        resultado.textContent = 'NEGATIVO'
+    } else {
+        resultado.textContent = 'ZERO'
+    }
+}
+
+botaoSomar.addEventListener('click', somar)
+botaoIdentificar.addEventListener('click', identificar)
